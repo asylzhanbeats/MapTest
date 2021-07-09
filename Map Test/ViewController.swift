7c22bd5e652a38e3dcf44c62c4f21e78e749f6eb
Assylzhan <asylzhanbeats@gmail.com>
2021-07-09 16:55:43 +0600

    Initial Commit

---
+++ Map Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Map Test
+//
+//  Created by Assylzhan Nurlybekuly on 09.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
